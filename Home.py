13da666f48355518c40c1b25765775df4a6515bc
Juan Perez <juan@gmail.com>
2026-08-29 16:27:36 -0300

    Primera etapa

--- Home.py
+++ Home.py
@@ -2,23 +2,28 @@
 #Implementar un sistema que gestione la reserva de turnos médicos para los profesionales de las distintas especialidades que atiende un centro de salud, utilizando
 #matrices, listas y diccionarios para mantener la información, almacenándola en archivos para permitir su posterior recuperación. Aplicar recursividad para realizar las
 #búsquedas de una manera ágil y flexible.
+
 import Turnero;
-
+eleccion = 0
 print("Bienvenido -- Sistema de Reserva de Turnos Médicos")
+while eleccion != 5:
+    eleccion = int (input(
+    "Ingrese: \n" 
+    " 1 para dar turnos \n"
+    " 2 para reprogramar un turno \n"
+    " 3 para cancelar un turno \n"
+    " 4 para listar los turnos de un paciente \n"
+    " 5 para salir \n"))
 
-print("¿Que desea hacer?")
-print("Ingrese: 1 para dar turnos, 2 para reprogramar un turno, 3 para cancelar un turno, 4 para listar los turnos de un paciente")
-eleccion = int (input())
-if eleccion == 1:
-    Turnero.darTurno
-else:
-    if eleccion == 2:
-        Turnero.reprogramarTurno
+    if eleccion == 1:
+        Turnero.darTurno()
+    elif eleccion == 2:
+        Turnero.reprogramarTurno()
+    elif eleccion == 3:
+        Turnero.cancelarTurno()
+    elif eleccion == 4:
+        Turnero.listarTurnos()
+    elif eleccion == 5:
+        print("saliendo")
     else:
-        if eleccion == 3:
-            Turnero.cancelarTurno
-        else:
-            if eleccion == 4:
-                Turnero.listarTurnos
-            else:
-                print("Ingresa una opcion valida.")
+        print("Opcion invalidad.")
--- Turnero.py
+++ Turnero.py
@@ -1,17 +1,51 @@
+profesionales = [
+    ["1", "Perez, Carolina", "Cardiologia"],
+    ["2", "Gomez, Alejandra", "Clinica Medica"],
+    ["3", "Lopez, Eduardo", "Traumatologia"]
+]
+
+turnos = [
+
+]
+
 def darTurno():
+    ok= False
+    esp = input("Ingrese ID Especialidad \n")
     print("Ingrese DNI del paciente")
     dni = input()
-    print("Ingrese fecha")
+    print("Ingrese fecha xx/xx/xx")
     fecha = input()
-
-
-    if ok :
-        print("Turno registrado correctamente")
-    else : print("Ocurrio un error al registrar el turno")
+    print("Ingrese hora xx:xx")
+    hora = input()
+    for p in profesionales: # chequeo si existe el id del profesional ingresado en la lista de profesionales
+        if(p[0] == esp):
+            turnos.append([dni, fecha, hora]) # por ahora la lista de turnos es global para todos los profesionales
+            print("Turno dado correctamente") # faltaria chequear antes de dar un turno si no existia un turno ya en ese horario
+            return
+    print("no se encontró un profesional con id de especialidad ingresada")
 
 def reprogramarTurno():
+    dni = input("Ingrese DNI del paciente")
+    fecha = input("Ingrese nueva fecha")
+    hora = input("Ingrese nueva hora")
+    for t in turnos:
+        if(t[0] == dni):
+            t[1] = fecha
+            t[2] = hora
+            print("Turno reprogramado")
+            return #salir del bucle y no seguir buscando
+    print("No se encontró un turno para el DNI del paciente ingresado")
+
+def cancelarTurno():
     print("Ingrese DNI del paciente")
     dni = input()
-    print("Ingrese nueva fecha")
-    fecha = input()
+    for t in turnos:
+        if(t[0] == dni):
+            turnos.remove(t)
+            print("Turno cancelado")
+            return #sale del bucle y no sigue buscando al pedo
+    print("No se encontró un turno para el paciente ingresado")
 
+def listarTurnos():
+    for t in turnos:
+        print("Paciente DNI: " + t[0] + " Fecha: " + t[1] + " Hora: " + t[2])
