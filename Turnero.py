640406412438ec701e76c42f274e43f9d9331a44
Luke <lucas21matu@gmail.com>
2026-09-18 15:44:45 -0300

    la lista de profesionales se cambio por un diccionario y mejore el calendario

--- Turnero.py
+++ Turnero.py
@@ -1,30 +1,23 @@
 import datetime
 import Calendario_de_Turnos
 
-profesionales = [
-    ["1", "Perez, Carolina", "Cardiologia"],
-    ["2", "Gomez, Alejandra", "Clinica Medica"],
-    ["3", "Lopez, Eduardo", "Traumatologia"]
-]
-
+# 
+profesionales = {"1": {"nombre": "Perez, Carolina","especialidad": "Cardiología"},"2": {"nombre": "Gomez, Alejandra","especialidad": "Clínica Médica"},"3": {"nombre": "Lopez, Eduardo","especialidad": "Traumatología"}}
 turnos = []
-
-
 # Para saber qué médicos están disponibles
 def mostrarProfesionales():
     """print("Los profesionales disponibles son: ")
     for p in profesionales:
         print("ID: " + p[0] + "\n Nombre: " + p[1] + "\n Especialidad: " + p[2])"""
-    
+
+
     print('-----------------------------------')
     print('|     Sacar Turno con          |')
-    print('| CARDIOLOGO ..........|1|')
-    print('| CLINICA MEDICA ....|2|')
-    print('| TRAUMATOLOGO ...|3|')
-
+    for id_prof in profesionales:
+        print(f"PRECIONE: {id_prof} si desea - Especialidad: {profesionales[id_prof]['especialidad']}.........{profesionales[id_prof]['nombre']}")
 
 def darTurno():
-    mostrarProfesionales()
+    mostrarProfesionales() #SI INGRESA UN VALOR Q NO SEA LOS MOSTRADOS IGUAL LO TOMA
 
     idProfesional = input("Ingrese el Profecional re-querido : ")
 
@@ -32,27 +25,25 @@ def darTurno():
     while not (len(dni) == 7 or len(dni) == 8) or not dni.isdigit():
         print("DNI no valido")
         dni = input("Ingrese nuevamente el DNI del paciente : ")
-
+#
     while True:
         try:
             Calendario_de_Turnos.mostrar_meses()
-            fecha = datetime.datetime.strptime(input("Ingrese la fecha xx/xx/xxxx"), "%d/%m/%Y")
+            fecha = datetime.datetime.strptime(input("Ingrese la fecha xx/xx/xxxx: "), "%d/%m/%Y")
             break
         except ValueError:
             print("Fecha incorrecta, ingrese la fecha de nuevo")
-
+#
     while True:
         try:
-            hora = datetime.datetime.strptime(input("Ingrese la hora H:M"), "%H:%M").time()
+            hora = datetime.datetime.strptime(input("Ingrese la hora H:M: "), "%H:%M").time()
             break
         except ValueError:
             print("Hora incorrecta, ingrese la hora de nuevo")
     for p in profesionales:  # chequeo si existe el id del profesional ingresado en la lista de profesionales
         if (p[0] == idProfesional):
-            turnos.append([dni, idProfesional, fecha,
-                           hora])  # por ahora la lista de turnos es global para todos los profesionales #Metería el idProfesional aquí también
-            print(
-                "Turno dado correctamente")  # faltaria chequear antes de dar un turno si no existia un turno ya en ese horario
+            turnos.append([dni, idProfesional, fecha,hora])  # por ahora la lista de turnos es global para todos los profesionales #Metería el idProfesional aquí también
+            print("Turno dado correctamente")  # faltaria chequear antes de dar un turno si no existia un turno ya en ese horario
             return
     print("no se encontró un profesional con id de especialidad ingresada")
 
@@ -100,4 +91,5 @@ def cancelarTurno():
 
 def listarTurnos():
     for t in turnos:
-        print("Paciente DNI: " + t[0] + "Profesional ID: " + t[1] + "Fecha: " + t[2].strftime("%d/%m/%Y") + " Hora: " + t[3].strftime("%H:%M"))
+        print()
+        print("Paciente DNI: " + t[0] + "\n Profesional ID: " + t[1] + "\n Fecha: " + t[2].strftime("%d/%m/%Y") + "\n Hora: " + t[3].strftime("%H:%M"))
