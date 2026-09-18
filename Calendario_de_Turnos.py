081dfdf15d9f196d49d924fb204fc4059a805585
Luke <lucas21matu@gmail.com>
2026-09-18 15:45:49 -0300

    se mejoro el calendario y ahora detecta años bisiestos
    Refactor calendar functions and improve month display.

--- Calendario_de_Turnos.py
+++ Calendario_de_Turnos.py
@@ -1,55 +1,43 @@
 from datetime import datetime
-
-
-
-def dias_de_semana():
-    for d in range(len(sem)):# dias_de_semana 
-        print(sem[d], end = ' 	')
-    print('')
-
-''' def elijo_turn():
-    print()
-    print()
-    diaT=int(input('Ingrese el Numero del dia que desea el Turno (ejemplo 12 = lunes): '))
-    print()
-    mesT=int(input('Ingrese el Numero del mes que desea el Turno (ejemplo 8 = agosto ): '))
-    return diaT,mesT,ahora.year
-'''
-dias = ["lunes","martes","miércoles","jueves","viernes","sábado","domingo"]
-sem = ['Lu','Ma','Mi','Ju','Vi','Sa','Do']
-meses = [" ","Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","AGOSTO","SEPTIEMBRE","OCTUBRE","NOVIEMBRE","DICIEMBRE"]
-
 ahora = datetime.now()
 fecha = datetime(ahora.year, ahora.month, ahora.day)
-'''print(dias[fecha.weekday()]) # imprime el dia de la semana '''
+#---------------------------------------FUNCIONES---------------------------------------#
+def ulti_dos_digit(año): # necesito los ultimos 2 digitos para la formula Doomsday
+        num = año // 100
+        new = año - num * 100
+        return new
+def bisiesto():
+    if (ahora.year % 4 == 0 and ahora.year % 100 != 0) or (ahora.year % 400 == 0):
+        return 29
+    return 28
+#----------------------------------LISTAS Y VARIABLES----------------------------------#    
+meses = [" ","Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre"]
+sem = ['0','Lu','Ma','Mi','Ju','Vi','Sa','Do']
+d_mes = [0,31, bisiesto(), 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
+
+dos_digit = ulti_dos_digit(ahora.year)
+dia_sem = int((dos_digit+(dos_digit/4))%7)
+#------------------------------------PROGRAMA PRINCIPAL----------------------------#
 
 def mostrar_meses ():
+    print('-------------------------------')
+    print('Mes Actual de',meses[ahora.month])
+    for t in range (1,len(sem)): # imprime dia de sem
+        print(sem[t], end = '     ')
     print()
- #mostramos el mes con dias disponibles 
-    print('	MES DE ',meses [ahora.month],'(',ahora.month ,')')
-
-# imprimir mes actual
-    dias_de_semana()
-
-    for i in range(ahora.day+1):
-        if i % 7 == 0 and i != 0:
-            print(i )
-        elif i !=0:
-            print(i, end = '	 '  )       
+    for d in range (dia_sem): #marca el indice del dia en que esta el mes
+       print('.',end = '        ')
+    s = dia_sem 
+    for i in range (ahora.day,d_mes[ahora.month]+1):#empezamos en 1 como todos los meses y agregamos 1 para q no termine antes el ciclo
+        if ahora.day <= i:
+            if ahora.day < i:
+                print(i,end = '      ')
+                s = s +1
+            elif ahora.day == i:
+                print('  X ',end = '      ')
+                s = s +1
+            if 'Do' == sem[s]:
+                print()
+                s = 0
     print()
-    print()
-
-# imprimir proximo mes 
-    print('	MES DE ',meses [ahora.month + 1],'(',ahora.month + 1,')')
-
-    dias_de_semana()
-    for x in range(ahora.day):
-        if x % 7 == 0 and x != 0:
-            print(x )
-        elif x !=0:
-            print(x, end = '	 '  )
-    print()
-
-
-    
-
